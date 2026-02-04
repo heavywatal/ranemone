04d5668512dd32bfe0903f06ef8d27fef199aa17
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2026-02-04 20:33:26 +0900

    feat: Calculate diversity indices

---
+++ R/aggregate.R
@@ -0,0 +1,55 @@
+#' Summarize community data by species, genus, family, etc.
+#'
+#' `ncopiesperml` is summed for in each sample.
+#' Species whose names contain "unidentified" are removed by default.
+#' @param community_df A community data frame,
+#'   e.g., output of `ranemone::read_tsv_xz("community_qc3nn_target.tsv.xz")`.
+#' @param .by Columns by which to summarize, e.g., `species`, `genus`, and/or `family`.
+#' @param rm_unidentified A logical value indicating whether to remove "unidentified" species.
+#' @returns `summarize_ncopies()` returns a data frame in longer format.
+#' @rdname aggregate
+#' @export
+#' @examples
+#' \dontrun{
+#' community_df = ranemone::read_tsv_xz("community_qc3nn_target.tsv.xz")
+#' species_df = ranemone::summarize_ncopies(community_df, .by = species)
+#' genus_df = ranemone::summarize_ncopies(community_df, .by = genus, rm_unidentified = FALSE)
+#'
+#' species_mat = ranemone::pivot_wider_ncopies(community_df, .by = species)
+#' family_mat = ranemone::pivot_wider_ncopies(community_df, .by = family, rm_unidentified = FALSE)
+#' }
+summarize_ncopies = function(community_df, .by = .data$species, rm_unidentified = TRUE) {
+  if (rm_unidentified) {
+    community_df = remove_unidentified(community_df)
+  }
+  dplyr::summarize(
+    community_df,
+    ncopiesperml = sum(.data$ncopiesperml, na.rm = TRUE),
+    .by = c(.data$samplename, {{ .by }})
+  )
+}
+
+#' @returns `pivot_wider_ncopies()` returns a community composition matrix
+#'   in wider format (samples x species).
+#' @rdname aggregate
+#' @export
+pivot_wider_ncopies = function(community_df, .by = .data$species, rm_unidentified = TRUE) {
+  if (rm_unidentified) {
+    community_df = remove_unidentified(community_df)
+  }
+  tidyr::pivot_wider(
+    community_df,
+    id_cols = .data$samplename,
+    names_from = {{ .by }},
+    names_sort = TRUE,
+    values_from = .data$ncopiesperml,
+    values_fill = 0,
+    values_fn = \(x) sum(x, na.rm = TRUE)
+  ) |>
+    tibble::column_to_rownames("samplename") |>
+    as.matrix()
+}
+
+remove_unidentified = function(community_df) {
+  dplyr::filter(community_df, !stringr::str_detect(.data$species, "unidentified"))
+}
---
+++ R/diversity.R
@@ -0,0 +1,66 @@
+#' Calculate alpha diversity indices
+#'
+#' Alpha diversity indices are calculated using `ncopiesperml` as a proxy of abundances.
+#'
+#' The output from these two functions should be equivalent given the same input.
+#' `summarize_alpha_diversity()` operates in a tidyverse style with longer data frames,
+#' while `calc_alpha_div()` operates on a community composition matrix in wider format
+#' using the [vegan](https://cran.r-project.org/package=vegan) package.
+#' @param species_df A data frame summarized by species,
+#'   e.g., output of [summarize_ncopies()].
+#' @param .by Passed to [dplyr::summarize()].
+#' @param .groups Passed to [dplyr::summarize()].
+#' @returns A data frame with alpha diversity indices.
+#' @rdname diversity
+#' @export
+#' @examples
+#' \dontrun{
+#' species_df = ranemone::summarize_ncopies(community_df, .by = species)
+#' res1 = ranemone::summarize_alpha_diversity(species_df)
+#'
+#' species_mat = ranemone::pivot_wider_ncopies(community_df, .by = species)
+#' res2 = ranemone::calc_alpha_div(species_mat)
+#'
+#' stopifnot(all.equal(res1, res2))
+#' }
+summarize_alpha_diversity = function(species_df, .by = .data$samplename, .groups = NULL) {
+  species_df |>
+    dplyr::summarize(y = calc_alpha(.data$ncopiesperml), .by = {{ .by }}, .groups = .groups) |>
+    tidyr::unpack(.data$y) |>
+    dplyr::mutate(
+      invsimpson = 1 / (1 - .data$simpson),
+      pielou = dplyr::if_else(.data$richness > 1, .data$shannon / log(.data$richness), NA_real_)
+    )
+}
+
+calc_alpha = function(x) {
+  total = sum(x)
+  if (total > 0) {
+    p = x / total
+    data.frame(
+      richness = sum(x > 0),
+      shannon = -sum(p * log(p)),
+      simpson = 1 - sum(p^2)
+    )
+  } else {
+    data.frame(richness = 0L, shannon = 0, simpson = 1)
+  }
+}
+
+#' @param mat A community composition matrix (samples x species),
+#'   e.g., output of [pivot_wider_ncopies()].
+#' @rdname diversity
+#' @export
+calc_alpha_div = function(mat) {
+  richness = vegan::specnumber(mat)
+  shannon = vegan::diversity(mat, index = "shannon")
+  data.frame(
+    samplename = rownames(mat),
+    richness = richness,
+    shannon = shannon,
+    simpson = vegan::diversity(mat, index = "simpson"),
+    invsimpson = vegan::diversity(mat, index = "invsimpson"),
+    pielou = dplyr::if_else(richness > 1, shannon / log(richness), NA_real_)
+  ) |>
+    tibble::new_tibble()
+}
